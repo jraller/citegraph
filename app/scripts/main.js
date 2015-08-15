2cae0be90d974a5e5ba2d9c4f66aa5af09cb0072
Jason Aller <jraller@gmail.com>
2015-08-15 16:06:42 -0700

    drawing sized circles and all connectors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,67 +3,103 @@
 
 'use strict';
 
-function drawGraph() {
-	var parseDate = {},
-		xScale = {},
-		yScale = {},
-		colorScale = {},
-		xAxis = {},
-		yAxis = {},
-		xLabel = {},
-		yLabel = {},
-		legend = {},
-		plot = {},
-		table = [],
-		chart = {};
+if (typeof citationJSON != 'undefined') {
 
-	d3.select('#chart')
-	.append('svg')
-	.attr('id', 'coverageChart')
-	.attr('height', '400px');
+	function drawGraph() {
+		var parseDate = {},
+			xScale = {},
+			yScale = {},
+			sizeScale = {},
+			colorScale = {},
+			xAxis = {},
+			yAxis = {},
+			xLabel = {},
+			yLabel = {},
+			legend = {},
+			cases = {},
+			connections = {},
+			coords = {},
+			point = {},
+			plot = {},
+			table = [],
+			chart = {};
 
-	parseDate = d3.time.format('%Y-%m-%dT%H:%M:%S').parse;
+		d3.select('#chart')
+		.append('svg')
+		.attr('id', 'coverageChart')
+		.attr('height', '400px');
 
-	xScale = new Plottable.Scales.Time();
-	yScale = new Plottable.Scales.Linear();
-	colorScale = new Plottable.Scales.Color();
+		parseDate = d3.time.format('%Y-%m-%dT%H:%M:%S').parse;
 
-	xAxis = new Plottable.Axes.Time(xScale, 'bottom');
-	xLabel = new Plottable.Components.AxisLabel('Time', 0);
-	yAxis = new Plottable.Axes.Numeric(yScale, 'left');
-	yLabel = new Plottable.Components.AxisLabel('Court Leaning', -90);
+		xScale = new Plottable.Scales.Time();
+		yScale = new Plottable.Scales.Linear();
+		sizeScale = new Plottable.Scales.ModifiedLog();
+		sizeScale.range([5,25]);
+		colorScale = new Plottable.Scales.Color();
 
-	legend = new Plottable.Components.Legend(colorScale).maxEntriesPerRow(5);
+		xAxis = new Plottable.Axes.Time(xScale, 'bottom');
+		xLabel = new Plottable.Components.AxisLabel('Time', 0);
+		yAxis = new Plottable.Axes.Numeric(yScale, 'left');
+		yLabel = new Plottable.Components.AxisLabel('Court GLeaning', -90);
 
-	plot = new Plottable.Components.Group();
+		legend = new Plottable.Components.Legend(colorScale).maxEntriesPerRow(5);
 
-	plot.append(new Plottable.Plots.Scatter()
-		.addDataset(new Plottable.Dataset(citationJSON.opinion_clusters))
-		.x(function (d) {
-			return parseDate(d.date_filed);
-		}, xScale)
-		.y(function (d) {
-			return d.citation_count;
-		}, yScale)
-		.size(function (d) {
-			return d.citation_count;
-		}, yScale)
-		.attr('stroke', colorScale.scale('Case'))
-	);
+		plot = new Plottable.Components.Group();
 
-	table = [
-		[null, null, legend],
-		[yLabel, yAxis, plot],
-		[null, null, xAxis],
-		[null, null, xLabel]
-	];
+		cases = new Plottable.Plots.Scatter()
+			.addDataset(new Plottable.Dataset(citationJSON.opinion_clusters))
+			.x(function (d) {
+				return parseDate(d.date_filed);
+			}, xScale)
+			.y(function (d) {
+				return d.citation_count;
+			}, yScale)
+			.size(function (d) {
+				return d.citation_count;
+			}, sizeScale)
+			.attr('stroke', colorScale.scale('Case'))
+		plot.append(cases);
 
-	chart = new Plottable.Components.Table(table);
+		connections = new Plottable.Plots.Line()
+			.x(function (d) {
+				return parseDate(d.x);
+			}, xScale)
+			.y(function (d) {
+				return d.y;
+			}, yScale)
+			.attr('stroke', colorScale.scale('Connector'))
+		plot.append(connections);
 
-	chart.renderTo('#coverageChart');
+		citationJSON.opinion_clusters.forEach(function (cluster) {
+			point = {};
+			point.date_filed = cluster.date_filed;
+			point.citation_count = cluster.citation_count;
+			coords[cluster.id] = point;
+		});
 
+		citationJSON.opinion_clusters.forEach(function (cluster) {
+			cluster.sub_opinions[0].opinions_cited.forEach(function (id) {
+				connections.addDataset(new Plottable.Dataset([
+					{x: coords[cluster.id].date_filed, y: coords[cluster.id].citation_count},
+					{x: coords[id].date_filed, y: coords[id].citation_count}
+				]));
+			});
+		});
+
+		table = [
+			[null, null, legend],
+			[yLabel, yAxis, plot],
+			[null, null, xAxis],
+			[null, null, xLabel]
+		];
+
+		chart = new Plottable.Components.Table(table);
+
+		chart.renderTo('#coverageChart');
+
+	}
+
+	$(document).ready(function() {
+		drawGraph();
+	});
 }
-
-$(document).ready(function() {
-	drawGraph();
-});
