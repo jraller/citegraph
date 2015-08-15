8449790eba4138740dea273a48fc64b7cf88d6ee
Jason Aller <jraller@gmail.com>
2015-08-15 12:28:43 -0700

    time axis working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,22 +22,22 @@ function drawGraph() {
 	.attr('id', 'coverageChart')
 	.attr('height', '400px');
 
-	parseDate = d3.time.format('%m%d%Y').parse;
+	parseDate = d3.time.format('%Y-%m-%dT%H:%M:%S').parse;
 
 	xScale = new Plottable.Scales.Time();
-	yScale = new Plottable.Scales.Category();
+	yScale = new Plottable.Scales.Linear();
 	colorScale = new Plottable.Scales.Color();
 
 	xAxis = new Plottable.Axes.Time(xScale, 'bottom');
 	xLabel = new Plottable.Components.AxisLabel('Time', 0);
-	yAxis = new Plottable.Axes.Category(yScale, 'left');
+	yAxis = new Plottable.Axes.Numeric(yScale, 'left');
 	yLabel = new Plottable.Components.AxisLabel('Court Leaning', -90);
 
 	legend = new Plottable.Components.Legend(colorScale).maxEntriesPerRow(5);
 
 	plot = new Plottable.Components.Group();
 
-	plot.append(new Plottable.Plots.Line()
+	plot.append(new Plottable.Plots.Scatter()
 		.addDataset(new Plottable.Dataset(citationJSON.opinion_clusters))
 		.x(function (d) {
 			return parseDate(d.date_filed);
@@ -45,7 +45,10 @@ function drawGraph() {
 		.y(function (d) {
 			return d.citation_count;
 		}, yScale)
-		.attr('stroke', colorScale.scale('Inflation'))
+		.size(function (d) {
+			return d.citation_count;
+		}, yScale)
+		.attr('stroke', colorScale.scale('Case'))
 	);
 
 	table = [
